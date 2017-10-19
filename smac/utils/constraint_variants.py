ac05da669f1c110bc90281ce7eded73196bd0373
Michael Rudolph <mrudolph@oxygen-technologies.de>
2017-10-19 10:28:15 +0200

    add constraint_variants.py

---
+++ smac/utils/constraint_variants.py
@@ -0,0 +1,14 @@
+'''
+Created on Sep 7, 2017
+
+@author: mrudolph
+'''
+from enum import Enum
+
+class ConstraintVariant(Enum):
+
+    """Class to define numbers for status types"""
+    VARIANT_1 = 1
+    VARIANT_2 = 2
+    VARIANT_3 = 3
+    NO = 4
